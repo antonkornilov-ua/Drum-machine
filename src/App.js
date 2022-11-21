1a0b7e7ae8c8323137bca70c7164e532b994c18b
antonkornilov-ua <83725472+antonkornilov-ua@users.noreply.github.com>
2022-11-21 21:24:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [activeKey, setActiveKey] = useState('');
+    useEffect(() => {
+        document.addEventListener('keydown', (event) => {
+          playSound(event.key.toUpperCase());
+        });
+    }, []);
+
+    const drumPads = [
+        {
+            keyCode: 81,
+            text: 'Q',
+            src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+        },
+        {
+            keyCode: 87,
+            text: 'W',
+            src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+        },
+        {
+            keyCode: 69,
+            text: 'E',
+            src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+        },
+        {
+            keyCode: 65,
+            text: 'A',
+            src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+        },
+        {
+            keyCode: 83,
+            text: 'S',
+            src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+        },
+        {
+            keyCode: 68,
+            text: 'D',
+            src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+        },
+        {
+            keyCode: 90,
+            text: 'Z',
+            src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+        },
+        {
+            keyCode: 88,
+            text: 'X',
+            src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+        },
+        {
+            keyCode: 67,
+            text: 'C',
+            src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+        },
+    ];
+
+    function playSound(selector) {
+        const audio = document.getElementById(selector);
+        audio.play();
+        setActiveKey(selector);
+    }
+    return (
+        <div className="App">
+            <div id="drum-machine">
+                <div className="drum-pads">
+                    {drumPads.map((drumPad) => (
+                        <div
+                            onClick={() => {
+                                playSound(drumPad.text);
+                            }}
+                            className="drum-pad"
+                            id={drumPad.src}
+                            key={drumPad.keyCode}>
+                            {drumPad.text}
+                            <audio preload="auto" className="clip" id={drumPad.text} src={drumPad.src}></audio>
+                        </div>
+                    ))}
+                </div>
+                <div id="display">{`You pressed \n ${activeKey}`}</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
